be200e4179cfdc699a5091fba0b972272f8d516e
yolocat-dev <developer@yolocat.dev>
2024-03-27 19:29:05 +0100

    refactor: cleaned up code and suppressed wrong warnings

--- src/main/java/dev/yolocat/cpptranspiler/data/CppFile.java
+++ src/main/java/dev/yolocat/cpptranspiler/data/CppFile.java
@@ -30,30 +30,35 @@ public class CppFile {
 
         private String path = "";
         private String name = "";
-        private List<String> imports = new ArrayList<>();
-        private List<CppClass> classes = new ArrayList<>();
-        private List<CppInterface> interfaces = new ArrayList<>();
+        private final List<String> imports = new ArrayList<>();
+        private final List<CppClass> classes = new ArrayList<>();
+        private final List<CppInterface> interfaces = new ArrayList<>();
 
+        @SuppressWarnings("UnusedReturnValue")
         public Builder path(String path) {
             this.path = path;
             return this;
         }
 
+        @SuppressWarnings("UnusedReturnValue")
         public Builder name(String name) {
             this.name = name;
             return this;
         }
 
+        @SuppressWarnings("UnusedReturnValue")
         public Builder imports(String... imports) {
             this.imports.addAll(List.of(imports));
             return this;
         }
 
+        @SuppressWarnings("UnusedReturnValue")
         public Builder classes(CppClass... classes) {
             this.classes.addAll(List.of(classes));
             return this;
         }
 
+        @SuppressWarnings("UnusedReturnValue")
         public Builder interfaces(CppInterface... interfaces) {
             this.interfaces.addAll(List.of(interfaces));
             return this;
